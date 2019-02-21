4908dd4d90340a1b315abfa6f912c36153f7ad7c
pathganker <junliangleo@gmail.com>
2019-02-22 02:01:25 +0800

    login

--- jackblog-vue/src/components/Login/index.vue
+++ jackblog-vue/src/components/Login/index.vue
@@ -4,31 +4,34 @@
     <div class="signin-container">
       <h4 class="title">登 录</h4>
       <div id="signinForm">
-        <form class="signin-form form-horizontal" ref="signinForm" :model="siginForm" :rules="siginRules">
+        <form class="signin-form form-horizontal">
           <div class="form-group">
             <div class="input-group">
               <div class="input-group-addon">
                 <i class="fa fa-envelope-o"></i>
               </div>
-              <input type="text" name="username" v-model="siginForm.username" data-vv-delay="100" class="form-control" placeholder="邮箱"></input>
+              <input type="text" name="username" data-vv-as="用户名" v-model="siginForm.username" v-validate="'required|username'" data-vv-delay="100" class="form-control" placeholder="邮箱"></input>
             </div>
+            <span class="tip-span">{{ errors.first('username') }}</span>
+            <span class="tip-span">{{ errors.first('required') }}</span>
           </div>
           <div class="form-group">
             <div class="input-group">
               <div class="input-group-addon"><i class="fa fa-unlock-alt"></i></div>
-              <input type="password" name="password" v-model="siginForm.password" class="form-control" placeholder="密码"></input>
+              <input type="password" name="password" data-vv-as="密码" v-model="siginForm.password" v-validate="'required|password'" class="form-control" placeholder="密码"></input>
             </div>
+            <span class="tip-span">{{ errors.first('password') }}</span>
           </div>
           <div class="form-group">
             <div class="col-xs-6 captcha-code">
-              <input type="text" name="captcha" v-model="siginForm.captcha" maxlength="6" class="form-control" placeholder="验证码"></input>
+              <input type="text" name="captcha" data-vv-as="验证码" v-model="siginForm.captcha" v-validate="'required|captcha|min:4'" maxlength="6" class="form-control" placeholder="验证码"></input>
             </div>
             <div class="col-xs-6 captcha-img">
               <a href="javascript:;" @click.prevent="getCaptchaUrl()">
                 <img :src="captchaUrl"></img>
               </a>
             </div>
-  
+          <span class="tip-span">{{ errors.first('captcha') }}</span>
           </div>
           <div class="form-group">
             <button class="btn btn-primary btn-lg btn-block" @click.prevent="login('siginForm')" id="signin_btn">登 录</button>
@@ -45,10 +48,8 @@
 </template>
 
 <script>
-import { Validator } from 'vee-validate'
 import snsloginbtns from './snsLogin'
 import { mapState, mapActions } from 'vuex'
-import {validateEmail} from '../../utils/validate'
 export default {
   components: {
     snsloginbtns
@@ -61,30 +62,11 @@ export default {
     })
   },
   data() {
-    const validateUsername = (rule, value, callback) => {
-      callback( this.showMsg("请输入符合格式的用户名",'error'));
-      if (!validateEmail(value)) {
-        
-      } else {
-        callback();
-      }
-    }
-    const validateCaptcha = (rule, value, callback) => {
-       callback();
-    }
     return {
       siginForm: {
         username: 'zankokutenshi@yeah.net',
         password: '123456',
         captcha: ''
-      },
-      siginRules:{
-        username: [
-            { required: true, trigger: 'blur', validator: validateUsername }
-        ],
-        captcha: [
-            { required: true, trigger: 'blur', validator: validateCaptcha }
-        ]
       }
     }
   },
--- jackblog-vue/src/validators/index.js
+++ jackblog-vue/src/validators/index.js
@@ -2,16 +2,28 @@ import Vue from 'vue'
 import VeeValidate from 'vee-validate'
 import { Validator } from 'vee-validate'
 
+Validator.remove('required')
+Validator.extend('required',{
+  getMessage: field => '请输入'+ field,
+  validate: value => !!(value)
+})
 Validator.extend('nickname', {
   getMessage: field => '请输入正确的呢称(2-15字符，中英文、数字和下划线)',
   validate: value => /^[(\u4e00-\u9fa5)0-9a-zA-Z\_\s@]+$/.test(value)
 })
-
+Validator.extend('username', {
+  getMessage: field => '请输入正确的邮箱',
+  validate: value => /^[a-z0-9](?:[-_.+]?[a-z0-9]+)*@([a-zA-Z0-9]+[_|\_|\.]?)*[a-zA-Z0-9]+\.[a-zA-Z]{2,3}$/.test(value)
+})
+Validator.extend('captcha', {
+  getMessage: field => '验证码错误',
+  validate: value => true
+})
 const config = {
   errorBagName: 'errors', // change if property conflicts.
   fieldsBagName: 'fields', 
   delay: 0, 
-  locale: 'en', 
+  locale: 'zh_CN', 
   dictionary: null, 
   strict: true, 
   enableAutoClasses: true,
